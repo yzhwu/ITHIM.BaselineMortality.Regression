8b281848f6ebd955a324889bbc7e7f9eaa2cbfc6
Yizheng Wu <Yizheng@campus-015-251.ucdavis.edu>
2018-03-15 11:05:31 -0700

    reorganize

--- R/DataPreparation.R
+++ R/DataPreparation.R
@@ -1,10 +1,11 @@
 # ITHIM Project
-# Regression analysis
+# Regression analysis - Data preparation
 
 # library difinition
 library(tidycensus)
 library(tidyverse)
 library(viridis)
+library(foreign)
 
 # install the census api key
 # obtain the key from https://api.census.gov/data/key_signup.html
@@ -23,9 +24,11 @@ v.acs5.11 <- load_variables(2011,"acs5")
 View(v.decenial.10)
 View(v.acs5.11)
 
-##################### extract the variables from 2010 sf1 (by zcta)
+##################### Part I. extract the variables from 2010 sf1 (by zcta) ###############
 ### total population
 population.total <- get_decennial(geography = "zcta",variables = "P0010001",year = 2010,state = "CA")
+
+zcta.CA.all <- unique(population.total$GEOID[which(population.total$GEOID%in%c(90001:96162))])
 
 ### population - sex by age
 
@@ -83,7 +86,7 @@ pop.race.percentage <- cbind(pop.white$value/population.total$value,
                              pop.black$value/population.total$value,pop.Hisp$value/population.total$value)
 colnames(pop.race.percentage) <- c("White%","Black%","Hisp%")
 
-##################### extract the variables from ACS 5 years 2007-2011 (by zcta)
+##################### Part II. extract the variables from ACS 5 years 2007-2011 (by zcta) ###############
 ### Education
 # ID.1: less than high school graduate
 # ID.2: high school graduate (includes equivalency)
@@ -115,7 +118,6 @@ for (i in 1:length(variable.list.income)){
 pop.unemployed <- get_acs(geography = "zcta",variables = "B23025_005E",year = 2011,survey = "acs5")
 
 
-
 population.total <- get_acs(geography = "zcta",variables = "B16010_002E",year = 2011,survey = "acs5")
 
 population.total <- get_acs(geography = "county",variables = "B17001_002E",year = 2011,survey = "acs5",state = "CA")
@@ -123,3 +125,61 @@ population.total <- get_acs(geography = "county",variables = "B17001_002E",year 
 population.total
 a<-population.total[which(population.total$GEOID%in%c(90001:96162)),]
 class(a)
+
+##################### Part III. extract the death numbers from CDPH (by zcta) ###############
+
+# Set working drectory
+setwd("/Users/Yizheng/Documents/02_Work/17_ITHIM-Phase II/04_Data/00_baseline mortality/01_Regression")
+
+# input the data file
+
+cdph <- read.spss('TEMPB.sav',to.data.frame = TRUE)
+head(cdph)
+
+# Function Definition -------------------------------------------------------------------------
+processCDPH <- function(zip.code,year){
+  
+  # obtain the target data according to county of resident and year of death
+  cdph.local.year <- cdph[which(cdph$zipcode==zip.code & cdph$yod%in%year),]
+  
+  local.gbd <- matrix(NA,nrow = 16,ncol = 1)
+  
+  for (j in 1:2){ # sex
+    for (i in 1:8){ # age group
+      #obtain the all-cause mortality for each group
+      local.gbd[8*(j-1)+i,1] <- nrow(cdph.local.year[which(cdph.local.year$sex == j & cdph.local.year$age8cat == i),])
+    }
+  }
+  
+  
+  # defina the dimension names for both matrices
+  dimnames(local.gbd) = list(c(paste0("maleAgeClass ",1:8),
+                               paste0("femaleAgeClass ",1:8)),zip.code)
+  
+  # return the matrices
+  return(local.gbd)
+  
+}
+
+# Part 3 Output the result in .csv file --------------------------------------------------------
+
+# CA zip code list (range: 90000)
+
+no.zip <- length(zcta.CA.all)
+year <- c("2008","2009","2010")
+
+result <- NULL
+for (i in 1:10){
+  #print(i)
+  temp <- processCDPH(zip.code.all[i],year)
+  result <- cbind(result,temp)
+}
+
+write.csv(result/3,file = "CA_localGBD.allcause.ziplevel.csv")
+
+
+
+
+
+
+
